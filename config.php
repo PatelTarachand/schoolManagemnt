d3d8ce28dcb221a1f46a93623c52e946d68d848c
Patel050598 <kamleshp52170@gmail.com>
2024-02-15 16:27:03 +0530

    connection stablished to database

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    $connection=mysqli_connect("localhost",'root','','school_management');
+    if(!$connection){
+        echo "Connection failed";
+    }
+
+    date_default_timezone_set("Asia/Kolkata");
+    //echo  date_default_timezone_get();
+?>
